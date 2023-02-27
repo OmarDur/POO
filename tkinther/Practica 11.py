d3f503997bb9d6363f373ddab27b99fd7680c779
OmarDur <125851632+OmarDur@users.noreply.github.com>
2023-02-27 16:46:43 -0600

    agregamos color a nuestro cuadro

--- tkinther/Practica 11.py
+++ tkinther/Practica 11.py
@@ -4,3 +4,17 @@ from tkinter import Tk, Button, Frame
 ventana= Tk()
 ventana.title("Ejemplo de 3 frames ")
 ventana.geometry("600x400")
+
+#2.- Agregamos los frames
+seccion1= Frame(ventana, bg="blue")
+seccion1.pack(expand=True, fill="both")
+
+seccion2= Frame(ventana, bg="yellow")
+seccion2.pack(expand=True, fill="both")
+
+seccion3= Frame(ventana, bg="green")
+seccion3.pack(expand=True, fill="both")
+
+
+#LLamamos el Main
+ventana.mainloop()
