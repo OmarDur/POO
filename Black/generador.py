3a12c99cfb041aa836cfcb0414ac41ee5656f655
OmarDur <125851632+OmarDur@users.noreply.github.com>
2023-03-15 15:53:23 -0600

    c
    4

---
+++ Black/generador.py
@@ -0,0 +1,1 @@
+//How to generate a password generator in python?
--- tkinther/14.py
+++ tkinther/14.py
@@ -1,10 +1,16 @@
 from tkinther import *
 import random, string
 
+
 root=Tk()
 root.geometry("400x400")
 root.resinzable(0,0)
 root.title("Generador de Contraseñas")
 
+pass_label = Label(root, text = 'password lenght', font = 'arial 10 bold').pack()
+pass_len = IntVar()
+length = Spinbox(root, from_ =8, to_=32, textvariable = pass_len,width = 15).pack()
+
+pass_str =StringVar()
 
 
